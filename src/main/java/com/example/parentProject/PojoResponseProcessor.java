fe4cff289d2d734859aebf33453b610c732e81fb
GopinathMoorthy <gopinath5293@gmail.com>
2022-03-21 11:13:23 +0530

    modified pojoresponse

--- src/main/java/com/example/parentProject/PojoResponseProcessor.java
+++ src/main/java/com/example/parentProject/PojoResponseProcessor.java
@@ -2,8 +2,10 @@ package com.example.parentProject;
 
 import com.example.parentProject.dto.CustomerDetails;
 import org.apache.camel.Exchange;
+import org.apache.camel.ExchangeProperties;
 import org.apache.camel.Processor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class PojoResponseProcessor implements Processor {
@@ -11,7 +13,10 @@ public class PojoResponseProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
         System.out.println("Inside PojoResponseProcessor");
-        Object obj = exchange.getIn().getBody(String.class);
-        exchange.getMessage().setBody(obj);
+        CustomerDetails customerDetails = exchange.getIn().getBody(CustomerDetails.class);
+        Map<String,Object> headerMap = new HashMap<>();
+        headerMap.put(Exchange.HTTP_RESPONSE_CODE,customerDetails.getResponseCode());
+        exchange.getMessage().setHeaders(headerMap);
+        exchange.getMessage().setBody(customerDetails.getResponseData());
     }
 }
